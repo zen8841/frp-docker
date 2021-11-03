38f0b181b717937e94dac1b499b67297ba01ad72
zen wen <zen@Zen-Arch-Desktop.localdomain>
2021-11-03 22:26:31 +0800

    add some file

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # frpc-docker
 A simple [frp](https://github.com/fatedier/frp) client docker image
 
-GitHub [zen8841/frpc-docker](https://github.com/zen8841/frpc-docker)
+GitHub [zen8841/frp-docker](https://github.com/zen8841/frp-docker)
+
+Docker [zen8841/frps](https://hub.docker.com/r/zen8841/frps)
 
 Docker [zen8841/frpc](https://hub.docker.com/r/zen8841/frpc)
 ## Usage
 docker cli:
 ```shell
-docker run -v /path/to/frpc.ini:/etc/frpc.ini zen8841/frpc
+# frps
+docker run \
+	-v /path/to/frps.ini:/etc/frps.ini \
+	-p #what port you want to expose \
+	zen8841/frps
+#frpc
+docker run \
+	-v /path/to/frpc.ini:/etc/frpc.ini \
+	zen8841/frpc
 ```
-You can modify the Dockerfile to create your own frps or frpc docker image
+You can put frps/frpc into same folder as Dockerfile to build your own frps/frpc docker image
--- update.sh
+++ update.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+# Auto update docker image
+work_DIR=$HOME/Project/frp-docker
+version=$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep "browser_download_url.*_linux_amd64.tar.gz" | cut -d '_' -f 4)
 systemctl start docker
-VERSION=$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep "browser_download_url.*_linux_amd64.tar.gz" | cut -d '_' -f 4)
-docker build /home/zen/Project/frpc-docker -t zen8841/frpc:$VERSION
-docker image tag zen8841/frpc:$VERSION zen8841/frpc:latest
-docker push zen8841/frpc:$VERSION zen8841/frpc:latest
-docker image rm zen8841/frpc:latest zen8841/frpc:$VERSION alpine
+cd $work_DIR
+curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep "browser_download_url.*_linux_amd64.tar.gz" | cut -d '"' -f 4 | wget -qi - && tar -xzf frp_*
+cp frp_*/frps ./frps/
+cp frp_*/frpc ./frpc/
+docker build $work_DIR/frps -t zen8841/frps:$version
+docker build $work_DIR/frpc -t zen8841/frpc:$version
+docker image tag zen8841/frps:$version zen8841/frps:latest
+docker image tag zen8841/frpc:$version zen8841/frpc:latest
+docker push zen8841/frps:$version
+docker push zen8841/frps:latest
+docker push zen8841/frpc:$version
+docker push zen8841/frpc:latest
+docker image rm zen8841/frpc:latest zen8841/frpc:$version zen8841/frps:latest zen8841/frps:$version busybox
 systemctl stop docker
+rm -r frp_* frp?/frp*
